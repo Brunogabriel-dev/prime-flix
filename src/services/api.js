78960e34f9675c8f24a8726849eb2229d0926b52
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-07 12:21:46 -0300

    Inclusão de texto

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// Base da URL:https://api.themoviedb.org/3/
+// URL DA API:/movie/now_playing?api_key=c458a608893b2b6a6d6a339e7feada62&language=pt-br
+
+const api = axios.create({
+
+});
