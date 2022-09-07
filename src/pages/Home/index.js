03e17cae5fa17f1aab9f2cb212aae00e38356c17
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-07 19:13:55 -0300

    Inclusão de texto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 // URL DA API:/movie/now_playing?api_key=c458a608893b2b6a6d6a339e7feada62&language=pt-br
 
@@ -33,6 +35,8 @@ function Home(){
           return(
             <article key={filme.id}>
               <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
             </article>
           )
         })}
