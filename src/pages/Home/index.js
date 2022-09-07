7085992ce78e08e1d87e0e2b74f3ee2ce225a514
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-07 12:30:38 -0300

    Inclusão de texto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react';
+
+// URL DA API:/movie/now_playing?api_key=c458a608893b2b6a6d6a339e7feada62&language=pt-br
 
 function Home(){
+  const [filmes, setFilmes] = useState([]);
+
+
+  useEffect(()=>{
+
+    async function loadFilmes(){
+
+    }
+
+    loadFilmes();
+
+  }, [])
+
   return(
     <div>
       <h1>BEM VINDO A HOME</h1>
--- src/services/api.js
+++ src/services/api.js
@@ -4,5 +4,7 @@ import axios from 'axios';
 // URL DA API:/movie/now_playing?api_key=c458a608893b2b6a6d6a339e7feada62&language=pt-br
 
 const api = axios.create({
-
+  baseURL: 'https://api.themoviedb.org/3/'
 });
+
+export default api;
