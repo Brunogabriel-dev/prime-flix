d0a0d43b794d27e437c6fe3d6d6ca094f692101e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-07 21:04:56 -0300

    Inclusão de texto

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,8 +1,37 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import api from '../../services/api';
+
 function Filme(){
   const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    async function loadFilme(){
+      await api.get(`/movie/${id}`, {
+        params:{
+          api_key: "c458a608893b2b6a6d6a339e7feada62",
+          language: "pt-BR",
+        }
+      })
+      .then((response)=>{
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(()=>{
+        console.log("FILME NAO ENCONTRADO")
+      })
+    }
+
+    loadFilme();
+
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO")
+    }
+  }, [])
 
   return(
     <div>
