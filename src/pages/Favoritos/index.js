1629606a0152df5e7e76f9600c58e03a48932a01
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-09 20:38:29 -0300

    Inclusão de texto

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './favoritos.css'
+import { Link} from 'react-router-dom'
 
 function Favoritos(){
   const [filmes, setFilmes] = useState([])
@@ -20,6 +21,10 @@ function Favoritos(){
           return(
             <li key={item.id}>
               <span>{item.title}</span>
+              <div>
+                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                <button>Excluir</button>
+              </div>
             </li>
           )
         })}
